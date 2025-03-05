141300f878570df7292d82494a9c31d7a71d9fad
ricardocosta23 <ricardo_costa23@outlook.com>
2025-03-05 09:40:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -281,13 +281,107 @@ def set_subitem_date():
 @app.route('/change_subitem_date', methods=['POST'])
 def change_subitem_date():
     if request.method == 'POST':
-        data = request.get_json()
-        challenge = data['challenge']
+        webhook_data = request.get_json()
+        print("Received webhook data:", webhook_data)
 
-        return jsonify({'challenge': challenge})
+        try:
+            board_id = webhook_data.get('event', {}).get('boardId', {})
+            item_id = webhook_data.get('event', {}).get('pulseId', {})
+            pulse_name = webhook_data.get('event', {}).get('pulseName', {})
+            print("Pulse:", pulse_name)
 
-        # print(request.json)
-        # return 'success', 200
+            if isinstance(pulse_name, str):
+                pulse_name = pulse_name.replace('"', '\\"')
+
+            if not board_id:
+                print("Error: Could not extract board_id or item_id from webhook.")
+                return jsonify({"error": "Missing board_id or item_id in webhook"}), 400
+
+            headers = {"Authorization": API_KEY, "Content-Type": "application/json"}
+
+            print("item é:", item_id)
+
+            subitem1_query = f"""query {{ items (ids: ["{item_id}"]) {{ column_values (ids: ["texto_1_mkncnqc9"]) {{ ... on TextValue {{ text }} }} }} }}"""
+            response = requests.post(url=API_URL, json={'query': subitem1_query}, headers=headers)
+            subitem1 = response.json()
+            subitem1_value = int(subitem1['data']['items'][0]['column_values'][0]['text'])
+            
+            print("Coluna subitem 1 é:", subitem1_value)
+            
+
+            subitem2_query = f"""query {{ items (ids: ["{item_id}"]) {{ column_values (ids: ["texto_2_mkncg9ba"]) {{ ... on TextValue {{ text }} }} }} }}"""
+            response = requests.post(url=API_URL, json={'query': subitem2_query}, headers=headers)
+            subitem2 = response.json()
+            subitem2_value = int(subitem2['data']['items'][0]['column_values'][0]['text'])
+            print("Coluna subitem 2 é:", subitem2_value)
+
+            subitemboard_query = f"""query {{ items (ids: ["{item_id}"]) {{ column_values (ids: ["texto_mknc26v7"]) {{ ... on TextValue {{ text }} }} }} }}"""
+            response = requests.post(url=API_URL, json={'query': subitemboard_query}, headers=headers)
+            subitemboard = response.json()
+            subitemboard_value = int(subitemboard['data']['items'][0]['column_values'][0]['text'])
+            print("Subitem board é:", subitemboard_value)
+
+            date_query = f"""query {{items (ids: [{item_id}]) {{ column_values {{ ... on DateValue {{ date }} }} }}}}"""
+            response = requests.post(url=API_URL, json={'query': date_query}, headers=headers)
+            date_data = response.json()
+
+            
+            try:
+                date_value = ""
+                column_values = date_data['data']['items'][0]['column_values']
+                for column in column_values:
+                    if 'date' in column:
+                        date_value = column['date']
+                        break
+            except (KeyError, IndexError):
+                date_value = ""
+                print ("datevalue: " , date_value)
+            
+
+            current_date = datetime.strptime(date_value, '%Y-%m-%d')
+            print ("currenttime:" , current_date)
+
+            update_query = f"""
+            mutation {{
+              change_multiple_column_values (
+                item_id: {subitem1_value},
+                board_id: {subitemboard_value},
+                column_values: "{{\\\"dup__of_data_mkmx6xcr\\\": \\\"{date_value}\\\", \\\"label_mkncg3sn\\\": \\\"Update date\\\"}}"
+              ) {{
+                id
+              }}
+            }}
+            """
+            data = {'query': update_query}
+            response = requests.post(url=API_URL, json=data, headers=headers)
+
+
+           
+            update_query = f"""
+            mutation {{
+              change_multiple_column_values (
+                item_id: {subitem2_value},
+                board_id: {subitemboard_value},
+                column_values: "{{\\\"dup__of_data_mkmx6xcr\\\": \\\"{date_value}\\\", \\\"label_mkncg3sn\\\": \\\"Update date\\\"}}"
+              ) {{
+                id
+              }}
+            }}
+            """
+            data = {'query': update_query}
+            response = requests.post(url=API_URL, json=data, headers=headers)
+
+            return jsonify({"success": True})
+
+        except (KeyError, TypeError, AttributeError) as e:
+            print(f"Error processing webhook data: {e}")
+            return jsonify({"error": "Error processing webhook data"}), 400
+        except requests.exceptions.RequestException as e:
+            print(f"Error communicating with Monday.com API: {e}")
+            return jsonify({"error": "Error communicating with Monday.com API"}), 500
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            return jsonify({"error": "An unexpected error occurred"}), 500
     else:
         abort(400)
 
