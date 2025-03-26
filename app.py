9b35a69589c1e4d580fbb31b730fe2e23824f8c9
ricardocosta23 <ricardo_costa23@outlook.com>
2025-03-26 11:08:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -397,7 +397,4 @@ if request.method == 'POST':
         # return 'success', 200
     else:
         abort(400)        
-        
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+     
